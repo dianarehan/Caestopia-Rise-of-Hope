1cf87f8033700c54412efd3a4f40eaba6d9511f6
MartinMelad <martinmelad66@gmail.com>
2024-02-19 03:26:30 +0200

    Test

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.U2D;
+using System;
 
 public class Test : MonoBehaviour
 {
@@ -12,19 +15,119 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        /* List<Dialogue> list = new List<Dialogue>()
+         {
+             new Dialogue("Rick", "What the Hell is this !!!", rick),
+             new Dialogue("Morty", "Wubba Lubba Dub Dub", morty),
+             new Dialogue("Batman", "What is this mess", batman)
+         };
+
+         DialogueManager.ShowDialogue(list);*/
+
         List<Dialogue> list = new List<Dialogue>()
         {
-            new Dialogue("Rick", "What the Hell is this !!!", rick),
-            new Dialogue("Morty", "Wubba Lubba Dub Dub", morty),
-            new Dialogue("Batman", "What is this mess", batman)
+           new Dialogue("Royal counsler", "my lord, I’ll Help you rule the City" +
+            " but you have to make some tough choices, do you understand?", rick),
         };
-
-        DialogueManager.ShowDialogue(list);
+        List<Choice> choices = new List<Choice>();
+        choices.Add(new Choice("YES", Yes1));
+        choices.Add(new Choice("NO", No1));
+        DialogueManager.ShowDialogue(list,"YES Or NO ?", choices);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator StartDialogue(UnityAction actione)
+    {
+        yield return new WaitForSeconds(5);
+        actione.Invoke();
+
+    }
+    void Yes1()
+    {
+        List<Dialogue> list = new List<Dialogue>()
+        {
+           new Dialogue("King", "Our city is now small and have some issues but it has the potential to grow.", rick),
+           new Dialogue("King", "for now you have to keep the citizens happy and grow our city .", rick),
+           new Dialogue("King", "-good luck sir!! .", rick)
+        };
+        DialogueManager.ShowDialogue(list);
+        //StartCoroutine(StartDialogue(ShowAfter1));
+        ShowAfter1();
+    }
+    void No1()
+    {
+        List<Dialogue> list = new List<Dialogue>()
+        {
+           new Dialogue("King", "haha funny just give the people some answers.", rick),
+        };
+        DialogueManager.ShowDialogue(list);
+        StartCoroutine(StartDialogue(ShowAfter1));
+
+    }
+
+    void ShowAfter1()
+    {
+        List<Dialogue> list = new List<Dialogue>()
+        {
+           new Dialogue("Fisherman", " my boat is too tiny, i cant catch many fish" +
+           " to be sufficient for the people in the city, may i have some gold to upgrade it?", rick),
+        };
+        List<Choice> choices = new List<Choice>();
+        choices.Add(new Choice("YES", Yes2));
+        choices.Add(new Choice("NO", No2));
+        DialogueManager.ShowDialogue(list, "YES Or NO ?", choices);
+    }
+
+    void Yes2()
+    {
+        List<Dialogue> list = new List<Dialogue>()
+        {
+           new Dialogue("King", " I am continually humbled by your kindness and generosity. -10 gold +9 happiness", rick),
+        };
+        DialogueManager.ShowDialogue(list);
+        StartCoroutine(StartDialogue(ShowAfter2));
+    }
+    void No2 ()
+    {
+        List<Dialogue> list = new List<Dialogue>()
+        {
+           new Dialogue("King", "  oh man!!! -2 happiness", rick),
+        };
+        DialogueManager.ShowDialogue(list);
+        StartCoroutine(StartDialogue(ShowAfter2));
+    }
+    void ShowAfter2()
+    {
+        List<Dialogue> list = new List<Dialogue>()
+        {
+           new Dialogue("The builder", " I’d like to build a new granary. This could help expand" +
+           " our population, but it will cost you a little bit", rick),
+        };
+        List<Choice> choices = new List<Choice>();
+        choices.Add(new Choice("YES", Yes3));
+        choices.Add(new Choice("NO", No3));
+        DialogueManager.ShowDialogue(list, "YES Or NO ?", choices);
+    }
+
+    void Yes3()
+    {
+        List<Dialogue> list = new List<Dialogue>()
+        {
+           new Dialogue("King", "Very well, let's invest in our future. population +10 money -50 ", rick),
+        };
+        DialogueManager.ShowDialogue(list);
+    }
+    void No3()
+    {
+        List<Dialogue> list = new List<Dialogue>()
+        {
+           new Dialogue("King", "alright we will stick with what we have got… -1 happiness", rick),
+        };
+        DialogueManager.ShowDialogue(list);
     }
 }
