63f334d1ae9dd4394f084be5fa93b5583ad7850a
MartinMelad <martinmelad66@gmail.com>
2024-02-21 00:53:44 +0200

    Character Movement

---
+++ Assets/Scripts/Characters/CharacterMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform endTransform;
+    [SerializeField]
+    private Transform startTransform;
+
+    private bool isEntring = false;
+    private bool isLeaving = false;
+
+    float elapsedTrinsform = 0;
+
+    private Transform tempSwap;
+
+    private float characterSpeed = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = startTransform.position;
+        transform.localScale = startTransform.localScale;
+
+        isEntring = true;
+        // Run The Enter Animation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(startTransform.position, endTransform.position, elapsedTrinsform);
+        transform.localScale = Vector3.Lerp(startTransform.localScale, endTransform.localScale, elapsedTrinsform);
+        elapsedTrinsform += characterSpeed * Time.deltaTime;
+
+       
+
+        if (isEntring && elapsedTrinsform >= 1)
+        {
+            isEntring = false;
+            GetComponent<Interaction>().StartInteraction();
+            Leave();
+            // TernOf the Animation;
+        }
+
+        if (isLeaving && elapsedTrinsform >= 1)
+        {
+            isLeaving = false;
+            Destroy(gameObject);
+            // TernOf the Animation;
+        }
+    }
+
+    public void Leave()
+    {
+        tempSwap = new GameObject().transform;
+
+        tempSwap.position = startTransform.position;
+        tempSwap.localScale = startTransform.localScale;
+
+        startTransform.position = endTransform.position;
+        startTransform.localScale = endTransform.localScale;
+
+        endTransform.position = tempSwap.position;
+        endTransform.localScale = tempSwap.localScale;
+
+        Destroy(tempSwap.gameObject);
+
+        isLeaving = true;
+        elapsedTrinsform = 0;
+        // Run Leaving Animation;
+    }
+
+}
