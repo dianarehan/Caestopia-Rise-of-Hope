33f89e31955a0cda5e0eaec788ca79468a15ff2c
Diana Rehan <daiana.rehan.dr@gmail.com>
2024-02-20 17:37:21 +0200

    soundcheck

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,31 +7,43 @@ public class MainMenu : MonoBehaviour
 {
     public UIDocument document;
     // Start is called before the first frame update
-
+    AudioSource audioSource;
+    public AudioClip clip;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void Awake()
     {
         var root = document.rootVisualElement;
         var VolumeSlider = root.Q<SliderInt>("VolumeSlider");
         VolumeSlider.RegisterValueChangedCallback(evt =>
         {
+            float volumeValue = evt.newValue;
             Debug.Log(evt.newValue);
+            audioSource.volume = volumeValue/100;
         });
         var optionsButton = root.Q<VisualElement>("settings-button");
         var optionsContainer = root.Q<VisualElement>("OptionsContainer");
         optionsContainer.AddToClassList("hide");
         optionsButton.RegisterCallback<MouseUpEvent>(ev =>
         {
+            audioSource.PlayOneShot(clip);
+
             // Toggle the 'hide' class on optionsContainer when the button is clicked
             optionsContainer.ToggleInClassList("hide");
         });
         var startButton = root.Q<Button>("start-button");
         startButton.RegisterCallback<MouseUpEvent>(ev =>
         {
+            audioSource.PlayOneShot(clip);
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     ;        });
         var exitButton = root.Q<Button>("exit-button");
         exitButton.RegisterCallback<MouseUpEvent>(ev =>
         {
+            audioSource.PlayOneShot(clip);
+
             Debug.Log("bye");
             Application.Quit(); 
         });
